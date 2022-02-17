1ecab2ab45b4f926b3527f23c1907175e098aba6
Unibeam <44021048+unibeam98@users.noreply.github.com>
2022-02-17 17:47:54 +0800

    v1ch08

---
+++ main/java/CJV/v1ch08/pair3/Manager.java
@@ -0,0 +1,28 @@
+package CJV.v1ch08.pair3;
+
+public class Manager extends Employee
+{
+    private double bonus;
+
+    public Manager(String name, double salary, int year, int month, int day)
+    {
+        super(name, salary, year, month, day);
+        bonus = 0;
+    }
+
+    public double getSalary()
+    {
+        double baseSalary = super.getSalary();
+        return baseSalary + bonus;
+    }
+
+    public void setBonus(double b)
+    {
+        bonus = b;
+    }
+
+    public double getBonus()
+    {
+        return bonus;
+    }
+}
